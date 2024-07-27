87844bedcedbce28cac3315271537c2176ac215d
Florian Marrero Liestmann <f.m.liestmann@fx-ttr.de>
2024-07-27 21:42:32 +0200

    Updating config

--- inc/config.h
+++ inc/config.h
@@ -5,7 +5,7 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char font[] = "monospace:size=16";
+static char font[] = "terminus:size=16";
 static int borderpx = 2;
 #define histsize 2500
 /*
@@ -16,7 +16,7 @@ static int borderpx = 2;
  * 4: value of shell in /etc/passwd
  * 5: value of shell in config.h
  */
-static char shell[] = "/bin/mksh";
+static char shell[] = "/bin/zsh";
 static char *utmp = NULL;
 static char stty_args[] = "stty raw pass8 nl -echo -iexten -cstopb 38400";
 
