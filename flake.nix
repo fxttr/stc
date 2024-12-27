f669f4bfab86add09b0625984b85c6154282b826
Florian Marrero Liestmann <f.m.liestmann@fx-ttr.de>
2024-12-27 10:47:56 +0100

    Update flake

--- flake.nix
+++ flake.nix
@@ -3,11 +3,15 @@
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    nix-vscode-extensions.url = "github:nix-community/nix-vscode-extensions";
 
-    utils.url = "github:numtide/flake-utils";
+    nix-code = {
+      url = "github:fxttr/nix-code";
+      inputs.extensions.follows = "nix-vscode-extensions";
+    };
   };
 
-  outputs = { self, nixpkgs, ... }@inputs: inputs.utils.lib.eachSystem [
+  outputs = { self, nixpkgs, flake-utils, ... }@inputs: flake-utils.lib.eachSystem [
     "x86_64-linux"
     "i686-linux"
     "aarch64-linux"
@@ -41,9 +45,46 @@
 
             # Tools
             valgrind
+            nixpkgs-fmt
+
+
+            (inputs.nix-code.vscode.${system} {
+              extensions = with inputs.nix-code.extensions.${system}; [
+                bbenoist.nix
+                jnoortheen.nix-ide
+                mkhl.direnv
+                llvm-vs-code-extensions.vscode-clangd
+              ];
+            })
           ];
         };
 
-        defaultPackage = pkgs.callPackage ./default.nix { };
+        defaultPackage = pkgs.llvmPackages.stdenv.mkDerivation rec {
+          pname = "stc";
+          version = "0.1.0";
+
+          src = ./.;
+
+          prePatch = ''
+            sed -i "s@/usr@$out@" Makefile
+          '';
+
+          nativeBuildInputs = with pkgs; [ gnumake ];
+
+          buildInputs = with pkgs; [
+            xorg.libX11
+            xorg.libXft
+          ];
+
+          meta = with nixpkgs.lib; {
+            homepage = "https://github.com/fxttr/stc";
+            description = ''
+              A simple terminal";
+            '';
+            licencse = licenses.mit;
+            platforms = with platforms; linux;
+            maintainers = [ maintainers.fxttr ];
+          };
+        };
       });
 }
